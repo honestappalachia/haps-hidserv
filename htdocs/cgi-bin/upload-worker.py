adcc8765a9b85e63611fbb1555c32bac6bd4f93e
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-09-28 18:51:55 -0400

    Refactored upload-worker into functional style
    Needs testing

--- htdocs/cgi-bin/upload-worker.py
+++ htdocs/cgi-bin/upload-worker.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
+"""
+This beanstalk worker processes file uploads to the hidden service.
+
+When it receives a file, the web process copies it to a temporary directory and
+puts a job on the running beanstalk process storing the file's absolute path.
+
+The worker process regularly checks the beanstalk queue for jobs to process.
+If it finds one, it
+
+    1. Encrypts the file using the PUBLIC_KEY_ID public key
+    2. Copies it to Amazon S3
+    3. shreds the original file
+"""
+
 import sys, os
 import subprocess
 import beanstalkc
 import gnupg
+from mylogging import debug, info, warning, error, critical
 
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
@@ -12,13 +27,13 @@ PUBLIC_KEY_ID = ''  # public key to use to encrypt files
 		    # stored in gpg keyring
 AWS_ACCESS_KEY = ''
 AWS_SECRET_KEY = ''
-BUCKET_NAME = ''
+BUCKET_NAME = 'haps-dev'
 
 TEMP_DIR = "/tmp"
 
 try:
     from settings import *
-except:
+except ImportError:
     pass
 
 def whereis(program):
@@ -32,54 +47,96 @@ def whereis(program):
             return os.path.join(path, program)
     return None
 
-# Open connection to beanstalkd
-beanstalk = beanstalkc.Connection(host='localhost', port=11300)
+def encrypt_file(source_file, destination_dir, key):
+    '''
+    GPG-encrypts source_file with key, saving encrypted file to destination_dir
 
-# infinite loop, handle jobs as they come
-while(True):
-    # is there something in the queue?
-    if beanstalk.peek_ready():
-	job = beanstalk.reserve(timeout=0)
-	# job should be an absolute path to a file
-	assert os.path.isfile(job), "job does not reference a file: %s" % job
+    f	--  absolute path to the file to encrypt
+    destination_dir --	absolute path to directory to save encrypted file in
+    key	--  keyid of public key to use; must be saved in gpg keyring
 
-	# init gpg
-	gpg = gnupg.GPG() # use default - current user's home
-	public_keys = gpg.list_keys()
-	assert PUBLIC_KEY_ID in [key['keyid'] for key in public_keys], \
-	    "Could not find the specified PUBLIC_KEY_ID in keyring"
+    Returns path to the encrypted file
+    '''
+    # init gpg
+    gpg = gnupg.GPG() # use default - current user's home
+    public_keys = gpg.list_keys()
+    assert PUBLIC_KEY_ID in [key['keyid'] for key in public_keys], \
+	"Could not find the specified PUBLIC_KEY_ID in keyring"
 
-	source_file = job.body	# needs to be an absolute path to a file - assert?
-	enc_filename = source_file.split("/")[-1] + ".gpg"
-	enc_file_path = os.path.join(TEMP_DIR, enc_filename)
-	try:
-	    fp = open(source_file, 'rb')
-	    encrypted_data = gpg.encrypt_file(
-		fp, # file object to encrypt
-		PUBLIC_KEY_ID, # encrypt for this recipient's public key
-		# TODO: where to store encrypted files
-		output = enc_file_path # path to encrypted file
-	    )
+    # build encrypted filename and path
+    e_filename = source_file.split("/")[-1] + ".gpg"
+    ef_path = os.path.join(destination_dir, e_filename)
 
+    try:
+	fp = open(source_file, 'rb')
+	encrypted_data = gpg.encrypt_file(
+	    fp,			# file object to encrypt
+	    key,		# public key of recipient
+	    output = ef_path,	# path to encrypted file
+	)
+    except IOError as e:
+	error(e)
+    
+    return ef_path
+
+def upload_to_s3(local_file, bucket_name, key_name=None, acl='private'):
+    '''
+    Uploads local_file to bucket_name on Amazon S3
+
+    key_name is the "filename" on Amazon S3. We'll default to the local file's name if
+    no alternative key_name is specified.
+    '''
+    # connect to Amazon S3
+    conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
+    bucket = conn.create_bucket(bucket_name)
+    k = Key(bucket)
+    
+    if key_name:
+	k.key = key_name
+    else:
+	k.key = local_file.split("/")[-1]
+
+    k.set_contents_from_filename(local_file)
+    k.set_acl(acl)
+
+def shred_file(f):
+    '''
+    Uses the *nix command shred to seriously erase f
+    '''
+    # check to see if we have shred installed; should this be an
+    # assertionError, or a warning (and we default to rm)?
+    assert whereis("shred") is not None, "Please install shred."
+    process = subprocess.Popen(['shred', '-fuz', source_file], shell=False)
+    if process.wait() == 0: # wait for shred to complete, check return code
+	pass
+    else: # some kind of error occurred; handle
+	error("An error occurred in shredding the file: shred returned %s" % (process.returncode))
+
+def main():
+
+    # Open connection to beanstalkd
+    beanstalk = beanstalkc.Connection(host='localhost', port=11300)
+
+    # infinite loop, handle jobs as they come
+    while(True):
+
+	if beanstalk.peek_ready():  # is there something in the queue?
+	    job = beanstalk.reserve(timeout=0)
+	    # job.body should be an absolute path to a file
+	    assert os.path.isfile(job.body), \
+		"job.body does not reference a file: %s" % job.body
+
+	    # encrypt file
+	    ef_path = encrypt_file(job.body, TEMP_DIR, PUBLIC_KEY_ID)
 	    # upload to s3
-	    conn = S3Connection(AWS_ACCESS_KEY, AWS_SECRET_KEY)
-	    bucket = conn.create_bucket(BUCKET_NAME) # returns bucket if it already exists
-	    k = Key(bucket)
-	    k.key = enc_filename
-	    k.set_contents_from_filename(enc_file_path)
-	    k.set_acl('private')
+	    upload_to_s3(ef_path, BUCKET_NAME)
+	    # shred encrypted and original file
+	    shred(job.body)
+	    shred(ef_path)
 
-	    # shred
-	    # check to see if we have shred installed; should this be an
-	    # assertionError, or a warning (and we default to rm)?
-	    assert whereis("shred") is not None, "Please install shred."
-	    process = subprocess.Popen(['shred', '-fuz', source_file], shell=False)
-	    if process.wait() == 0: # wait for shred to complete, check return code
-		pass
-	    else: # some kind of error occurred; handle
-		print "An error occurred in shredding the file: shred returned %s" % (process.returncode)
-	    
-	except IOError as e:
-	    print e
+	    # we handled that job; remove it from the queue
+	    job.delete()
 
-    time.sleep(10)  # how often to check and handle a job
+	time.sleep(10)  # how often to check and handle a job
+
+if __name__ == "__main__": main()
